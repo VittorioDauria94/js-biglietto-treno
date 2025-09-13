b35a8fac824bb9c8e7a22e58ac97afb38472789d
VittorioDauria94 <dauriavittorio94@gmail.com>
2025-09-13 12:19:49 +0200

    feat: scritta logica del programma

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* Ragionamento
+* RACCOLTA DATI
+* - chiedere il numero di chilometri da percorrere
+* - chiedere l'età del passeggero
+* - creare la variabile del messaggio
+* - creare la variabile costo biglietto per chilometro
+* - creare la variabile sconto minorenni in decimali
+* - creare la variabile sconto over 65 in decimali
+* - creare la variabile prezzo biglietto finale con valore 0
+* LOGICA
+* trasformare la stringa del prompt età in numero
+* trasformare la stringa del prompt chilometri in numero
+* SE età del passeggero è > 65
+*    prezzo biglietto = (numero chilometri * costo biglietto per chilometro) * sconto over 65
+* ALTRIMENTI SE età del passeggero >= 18
+*   prezzo biglietto finale = numero chilometri * costo biglietto per chilometro
+* altrimenti
+*   prezzo biglietto finale = (numero chilometri * costo biglietto per chilometro) * sconto minorenni
+*  STAMPA
+*   messaggio = il tuo biglietto costerà € (prezzo biglietto finale)  
+*/
