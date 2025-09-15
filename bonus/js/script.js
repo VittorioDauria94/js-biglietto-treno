be205e4c219458102887f277ecb46096757a8b6f
VittorioDauria94 <dauriavittorio94@gmail.com>
2025-09-15 19:40:53 +0200

    feat: esercizio 2 bonus completo

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,27 +1,46 @@
 // Esercizio 1
 
+// let message = "";
+// let result = 0;
+// const num1Str = prompt("Scegli il primo numero");
+// const num2Str = prompt("Scegli il secondo numero");
+// const userChoise = prompt("Scrivi se vuoi la somma o la differenza dei due numeri");
+
+// const num1 = parseInt(num1Str);
+// const num2 = parseInt(num2Str);
+
+
+// if (isNaN(num1) || isNaN(num2)) {
+//     message = `Errore: inserisci solo numeri validi, ricarica la pagina.`;
+// } else {
+//     if (userChoise === "somma") {
+//         result = num1 + num2;
+//         message = `La somma dei due numeri è ${result}`;
+//     } else if (userChoise === "differenza") {
+//         result = num1 - num2;
+//         message = `La differenza dei due numeri è ${result}`;
+//     } else {
+//         message = "Errore: scrivere correttamente se si vuole la somma o la differenza, ricarica la pagina.";
+//     }
+// }
+
+// console.log(message);
+
+
+// Esercizio 2
+
 let message = "";
-let result = 0;
-const num1Str = prompt("Scegli il primo numero");
-const num2Str = prompt("Scegli il secondo numero");
-const userChoise = prompt("Scrivi se vuoi la somma o la differenza dei due numeri");
+let yearOfBirth = 0;
+const userAgeStr = prompt("Quanti anni hai?");
+const currentYear = new Date().getFullYear();
 
-const num1 = parseInt(num1Str);
-const num2 = parseInt(num2Str);
+const userAge = parseInt(userAgeStr);
 
-
-if (isNaN(num1) || isNaN(num2)) {
-    message = `Errore: inserisci solo numeri validi, ricarica la pagina.`;
+if (isNaN(userAge)) {
+    message = `Errore: inserisci correttamente la tua età numericamente, ricarica la pagina.`;
 } else {
-    if (userChoise === "somma") {
-        result = num1 + num2;
-        message = `La somma dei due numeri è ${result}`;
-    } else if (userChoise === "differenza") {
-        result = num1 - num2;
-        message = `La differenza dei due numeri è ${result}`;
-    } else {
-        message = "Errore: scrivere correttamente se si vuole la somma o la differenza, ricarica la pagina.";
-    }
+    yearOfBirth = currentYear - userAge;
+    message = `Sei nato/a nel ${yearOfBirth}`;
 }
 
 console.log(message);
